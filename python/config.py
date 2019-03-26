e3ca96aac4a33712eb776939f739f93e7e78053d
advaccaro <avaccaro@usc.edu>
2019-03-26 00:57:17 -0700

    Fix path

--- python/config.py
+++ python/config.py
@@ -2,7 +2,7 @@
 '''Holds the paths to the project geodatabase (.gdb), data directory, tmp 
 directory, and output directory.  Change these paths prior to running
 this analysis.'''
-gdb = "/path/to/project.gdb"
+gdb = "/path/to/project.gdb/"
 data_dir = "/path/to/data/directory/"
 tmp_dir = "/path/to/temp/directory/"
 out_dir = "/path/to/out/directory/"
